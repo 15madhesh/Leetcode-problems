1d9ffa034c32259e6baaffe5058cff928cd73055
Madhesh S <madheshit2027@gmail.com>
2026-02-11 19:46:17 +0530

    Time: 1 ms (99.84%) | Memory: 42.8 MB (49.44%) - LeetSync

---
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int mirrorDistance(int n) {
+        int r = 0,v = n;
+        while(n > 0){
+            r = (r * 10) + ( n % 10);
+            n /= 10;
+        }
+        return Math.abs(r - v);
+    }
+}
