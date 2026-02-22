c7a06d138a487a644b5486da3866be8d890286c9
Madhesh S <madheshit2027@gmail.com>
2026-02-22 09:33:28 +0530

    Time: 106 ms (10.51%) | Memory: 56.8 MB (7.94%) - LeetSync

---
+++ 4044-maximum-bitwise-xor-after-rearrangement/maximum-bitwise-xor-after-rearrangement.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String maximumXor(String s, String t) {
+        int cz = 0;
+        int co = 0;
+        for (int i = 0; i < t.length(); i++) {
+            if (t.charAt(i) - '0' == 1) {
+                co++;
+            } else {
+                cz++;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                if(co > 0){
+                    sb.append('1');
+                    co--;
+                }else{
+                    sb.append('0');
+                    cz--;
+                }
+            } else {
+                if(cz > 0){
+                    sb.append('0');
+                    cz--;
+                } else {
+                    sb.append('1');
+                    co--;
+                }
+            }
+        }
+        t = sb.toString();
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == t.charAt(i)){
+                res.append('0');
+            }else{
+                res.append('1');
+            }
+        }
+        return res.toString();
+    }
+}
