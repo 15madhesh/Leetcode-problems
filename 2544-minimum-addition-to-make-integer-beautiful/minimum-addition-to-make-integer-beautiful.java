aaeef09479f3b1e8b7309997a5495b9dd242a371
Madhesh S <madheshit2027@gmail.com>
2026-08-06 08:45:35 -0400

    Time: 0 ms (100.00%) | Memory: 42.4 MB (10.14%) - LeetSync

---
+++ 2544-minimum-addition-to-make-integer-beautiful/minimum-addition-to-make-integer-beautiful.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long sum(long n){
+        long sum = 0;
+        while(n != 0){
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+    public long makeIntegerBeautiful(long n, int target) {
+        long x = n;
+        long mul = 1;
+        while(sum(x) > target){
+            x = x / 10 + 1;
+            mul *= 10;
+        }
+        return x * mul - n;
+    }
+}
