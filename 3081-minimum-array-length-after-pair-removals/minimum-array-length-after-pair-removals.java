a4f579e7111b65fc3d2daa4f5c552a4d51d9e123
Madhesh S <madheshit2027@gmail.com>
2026-09-10 18:42:36 +0530

    Time: 5 ms (98.32%) | Memory: 136.4 MB (80.45%) - LeetSync

--- 3081-minimum-array-length-after-pair-removals/minimum-array-length-after-pair-removals.java
+++ 3081-minimum-array-length-after-pair-removals/minimum-array-length-after-pair-removals.java
@@ -1,14 +1,16 @@
 class Solution {
     public int minLengthAfterRemovals(List<Integer> nums) {
         int len = nums.size();
-        int res = len;
+        int res = 0;
         int i = 0;
         int j = (len + 1) / 2;
         while(i < len / 2 && j < len){
-            if(nums.get(i) < nums.get(j)) res-= 2;
-            i++;
+            if(nums.get(i) < nums.get(j)){
+                res += 2;
+                i++;
+            }
             j++;
         }
-        return res;
+        return len - res;
     }
 }
